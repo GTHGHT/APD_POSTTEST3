a1b34fd03bf8d7c3054b1f1765585c2860579623
GTHGHT <49431663+GTHGHT@users.noreply.github.com>
2020-11-17 09:41:24 +0800

    Add files via upload

--- perulangan10.py
+++ perulangan10.py
@@ -4,6 +4,6 @@ bilangan = int(input("Masukkan Bilangan : "))
 for i in range(sys.maxsize):
     if 10 ** i > bilangan:
         print(
-            f"Bilangan Pangkat 10 yang terdekat adalah {10**i}"
+            f"Bilangan 10^x yang terdekat adalah {10**i}"
         )
         break
